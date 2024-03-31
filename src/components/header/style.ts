228cc97e5f7db4ea2087b0cc9b59646741e4a96e
Yves Antônio <yves@gec.inatel.br>
2024-03-31 14:54:15 -0300

    feat: componente header
    Desenvolvimento do componente header, ainda completamente estático

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,7 +1,47 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.div`
-  height: 88px;
-  width: 1075px;
-  background-color: #bbbbbb;
+  height: 74px;
+  border-bottom: 1px solid #94a3b8;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
+export const HeaderInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  margin-right: 25px;
+`
+
+export const HeaderUser = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: end;
+
+  span:nth-child(1) {
+    font-size: 16px;
+    color: #1e293b;
+    font-weight: bold;
+  }
+
+  span:nth-child(2) {
+    font-size: 12px;
+    color: #656565;
+    font-weight: 500;
+  }
+`
+
+export const HeaderIcon = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+  background-color: #0159ea;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+
+  padding: 11px 10px;
 `
--- src/pages/admin/projectsSignIn/styles.ts
+++ src/pages/admin/projectsSignIn/styles.ts
@@ -3,6 +3,7 @@ import { styled } from 'styled-components'
 export const PageContainer = styled.div`
   display: flex;
   flex-direction: row;
+  overflow: hidden;
 `
 
 export const ProjectsContainer = styled.div`
