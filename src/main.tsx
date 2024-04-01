71a9a635b3a8e914c8b9595f70483eff9e728f08
Yves Antônio <yves@gec.inatel.br>
2024-04-01 16:42:05 -0300

    feat: main redirect

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
