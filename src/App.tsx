c0cfb31e9d00469a1c68bd29ca378763568b4708
Yves Antônio <yves@gec.inatel.br>
2024-04-03 10:06:00 -0300

    feature: login layout and first acess

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable react/no-children-prop */
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { LoginPage } from './pages/login'
-import { Layout } from './layout'
+import { Layout } from './pages/appLayout'
 import { AdminProjectsList } from './pages/admin/projectsList'
 import { AdminProjectsSignIn } from './pages/admin/projectsSignIn'
 import { AdminUsersList } from './pages/admin/usersList'
+import { LoginLayout } from './pages/loginLayout'
+import { LoginFirstAccess } from './pages/loginFirstAcess'
 
 export function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<LoginLayout children={<LoginPage />} />} />
+        <Route
+          path="/login/firstAcess"
+          element={<LoginLayout children={<LoginFirstAccess />} />}
+        />
         <Route
           path="admin/projectsList"
           element={<Layout children={<AdminProjectsList />} />}
---
+++ src/pages/loginLayout/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import loginImg from '../../assets/imgs/loginImg.svg'
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+`
+
+export const LoginImg = styled.div`
+  display: flex;
+  background-color: aqua;
+  width: 60%;
+  background-image: url(${loginImg});
+  background-size: cover;
+  justify-self: flex-end;
+`
+
+export const LoginContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  justify-content: space-between;
+
+  margin: 30px 80px;
+
+  img:nth-child(1) {
+    width: 123px;
+    height: auto;
+    margin-bottom: 20px;
+  }
+`
+
+export const LoginFooter = styled.div`
+  display: flex;
+`
